c2441d6dd9b9e3891743b44850a61ba2d1d75dd8
Jakub <35743730+jgimza@users.noreply.github.com>
2018-09-09 14:43:36 +0200

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
         $host = 'localhost';
-        $db_user = '15_drozdowski';
+        $db_user = '15_gimza';
         $db_pass = 'haslo';
-        $db_name = '15_drozdowski';
+        $db_name = '15_gimza';
 ?>
